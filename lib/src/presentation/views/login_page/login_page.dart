1f64b82db1f7661cb992d96626f6274fca6489a4
ErickDev <123skatesk8@gmail.com>
2023-04-04 11:49:32 -0400

    feat: integrate login

--- lib/src/presentation/views/login_page/login_page.dart
+++ lib/src/presentation/views/login_page/login_page.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:loomi_flutter_boilerplate/src/components/input_text.dart';
-import 'package:loomi_flutter_boilerplate/src/components/text_field_container.dart';
-import 'package:loomi_flutter_boilerplate/src/presentation/views/home/home.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get_it/get_it.dart';
 import 'package:loomi_flutter_boilerplate/src/components/input_password.dart';
-import 'package:loomi_flutter_boilerplate/src/presentation/views/signup/signup.dart';
+import 'package:loomi_flutter_boilerplate/src/components/input_text.dart';
+import 'package:loomi_flutter_boilerplate/src/external/models/login.dart';
+import 'package:loomi_flutter_boilerplate/src/presentation/usecases/i_get_login_uc.dart';
+import 'package:loomi_flutter_boilerplate/src/presentation/views/home/home.dart';
+import 'package:loomi_flutter_boilerplate/src/presentation/views/signup_page/signup_page.dart';
 import 'package:loomi_flutter_boilerplate/src/presentation/widgets/custom_button.dart';
-import 'package:loomi_flutter_boilerplate/src/presentation/widgets/search_bar_component.dart';
+import 'package:loomi_flutter_boilerplate/src/utils/authentication.dart';
 import 'package:loomi_flutter_boilerplate/src/utils/custom_colors.dart';
 import 'package:loomi_flutter_boilerplate/src/utils/fonts.dart';
-import '../../../utils/helpers/assets_helper.dart';
+import 'package:loomi_flutter_boilerplate/src/utils/helpers/assets_helper.dart';
+import 'package:loomi_flutter_boilerplate/src/utils/validators.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
-
+class LoginPage extends StatefulWidget {
   static const routeName = "login";
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void whenToConnect() async {
+    var response = await GetIt.I.get<IGetLoginUseCase>()(
+        data: Login(
+            email: _emailController.text, password: _passwordController.text));
+
+    Authentication.saveToken(response.accessToken);
+
+    Navigator.pushNamed(context, Home.routeName, arguments: response.toJson());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            decoration: BoxDecoration(color: CustomColors.purple),
+        body: Form(
+      key: _formKey,
+      child: Container(
+          decoration: BoxDecoration(color: CustomColors.purple),
+          height: double.infinity,
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -54,11 +80,24 @@ class Login extends StatelessWidget {
                         fontWeight: FontWeight.bold),
                   ),
                 ),
-                InputText(labelText: "E-mail", hintText: "e-mail"),
-                InputPassword(hintText: "senha", labelText: "Senha"),
+                InputText(
+                  labelText: "E-mail",
+                  hintText: "e-mail",
+                  controller: _emailController,
+                  validator: Validators.emailValidator(),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                InputPassword(
+                  hintText: "senha",
+                  labelText: "Senha",
+                  controller: _passwordController,
+                  validator: Validators.passwordValidator(),
+                ),
                 CustomButton(
                   onTap: () {
-                    Navigator.pushNamed(context, Home.routeName);
+                    if (_formKey.currentState!.validate()) {
+                      whenToConnect();
+                    }
                   },
                   text: "Login",
                   expanded: true,
@@ -75,7 +114,7 @@ class Login extends StatelessWidget {
                   children: [
                     GestureDetector(
                       onTap: () {
-                        Navigator.pushNamed(context, Signup.routeName);
+                        Navigator.pushNamed(context, SignupPage.routeName);
                       },
                       child: Text(
                         "Criar conta",
@@ -86,6 +125,8 @@ class Login extends StatelessWidget {
                   ],
                 )
               ],
-            )));
+            ),
+          )),
+    ));
   }
 }
