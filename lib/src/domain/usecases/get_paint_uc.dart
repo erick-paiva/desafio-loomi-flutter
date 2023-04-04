5d91f96d5f763a1b53edd7667ee4ea4ca34bbb67
ErickDev <123skatesk8@gmail.com>
2023-04-04 11:56:07 -0400

    feat: create the GetPaintUC

---
+++ lib/src/domain/usecases/get_paint_uc.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:loomi_flutter_boilerplate/src/domain/repositories/i_paint_repository.dart';
+import 'package:loomi_flutter_boilerplate/src/external/models/cart_model.dart';
+import 'package:loomi_flutter_boilerplate/src/presentation/usecases/i_get_paint_uc.dart';
+
+class GetPaintUC implements IGetPaintUseCase {
+  @override
+  Future<List<CartModel>> call() async {
+    return await GetIt.I.get<IPaintRepository>().getPaint();
+  }
+}
