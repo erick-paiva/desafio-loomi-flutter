9d063f28645409c3a2f9b1a85f5e2e01ef1ca82d
ErickDev <123skatesk8@gmail.com>
2023-04-04 11:53:41 -0400

    refactor: rename the LoginPage and SignupPage screens

--- lib/src/utils/routes.dart
+++ lib/src/utils/routes.dart
@@ -3,13 +3,13 @@ import 'package:loomi_flutter_boilerplate/src/presentation/views/custom_loader_s
 import 'package:loomi_flutter_boilerplate/src/presentation/views/description_of_paints/description_of_paints.dart';
 import 'package:loomi_flutter_boilerplate/src/presentation/views/example_screen.dart';
 import 'package:loomi_flutter_boilerplate/src/presentation/views/home/home.dart';
-import 'package:loomi_flutter_boilerplate/src/presentation/views/login/login.dart';
-import 'package:loomi_flutter_boilerplate/src/presentation/views/signup/signup.dart';
+import 'package:loomi_flutter_boilerplate/src/presentation/views/login_page/login_page.dart';
+import 'package:loomi_flutter_boilerplate/src/presentation/views/signup_page/signup_page.dart';
 
 final Map<String, WidgetBuilder> routes = {
   ExampleScreen.routeName: (_) => const ExampleScreen(),
-  Login.routeName: (_) => const Login(),
-  Signup.routeName: (_) => const Signup(),
+  LoginPage.routeName: (_) => LoginPage(),
+  SignupPage.routeName: (_) => SignupPage(),
   Home.routeName: (_) => const Home(),
   CustomLoaderScreen.routeName: (_) => const CustomLoaderScreen(),
 };
